67a5efed13117b34da39ffe6977a3ee401afcc06
anshul <anshulkumar123.iimt@gmail.com>
2024-12-03 00:35:30 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import {
+  View,
+  Text,
+  SafeAreaView
+} from 'react-native'
+
+function App(){
+  return(
+    <SafeAreaView>
+    <View>
+      <Text>Hello Anshul !</Text>
+      <Text>What's going on !</Text>
+      <Text>How to going you ReactNative Preparation !</Text>
+      <Text>let's enjoy !</Text>
+    </View>
+  </SafeAreaView>
+  )
+}
+
+export default App;
